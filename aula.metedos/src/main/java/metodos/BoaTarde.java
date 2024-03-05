b7c284a16ff2d3ea0321c494b423ea0e0b69dacd
oliveiraedgarryan <151791213+oliveiraedgarryan@users.noreply.github.com>
2024-03-05 19:50:32 -0300

    Delete aula.metedos directory

--- aula.metedos/src/main/java/metodos/BoaTarde.java
+++
@@ -1,9 +0,0 @@
-package metodos;
-
-public class BoaTarde {
-	public void mensagenTarde() {
-		System.out.println("Tenha oma ótima tarde, e aproveite o sol. ");
-		System.out.println("Abençoada por Deus, com paz e pão de queijo");
-	}
-
-}
--- aula.metedos/src/main/java/metodos/BomDia.java
+++
@@ -1,8 +0,0 @@
-package metodos;
-
-public class BomDia {
-
-	public void mensagemDia() {
-		System.out.println("Um ótimo dia, repleto de paz e oportunidades");
-	}
-}
